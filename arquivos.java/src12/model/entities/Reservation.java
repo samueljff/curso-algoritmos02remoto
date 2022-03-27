59d1128f153fbc93893ec2624e81722ab8fd1111
Samuel Fomseca <samueljosef3@gmail.com>
2022-03-27 13:27:32 -0300

    Reserva de hotel: exceptions

---
+++ arquivos.java/src12/model/entities/Reservation.java
@@ -0,0 +1,61 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	public Reservation() {
+	}
+
+	public Reservation(Integer rommNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = rommNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRommNumber() {
+		return roomNumber;
+	}
+
+	public void setRommNumber(Integer rommNumber) {
+		this.roomNumber = rommNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+
+	public void updatedates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new IllegalArgumentException("Reservation dates for update must be future dates");
+		}
+		if (!checkOut.after(checkIn)) {
+			throw new IllegalArgumentException("Check-out date must be after check-in date");
+		}
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", ckeck-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " nights";
+	}
+}
