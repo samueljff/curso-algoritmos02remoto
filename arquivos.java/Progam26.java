40e8fd1f5c1c8e38e99205082aeef167b09ef66d
Samuel Fomseca <samueljosef3@gmail.com>
2022-03-29 22:10:56 -0300

    Listando caminh0s de pastas, arquivos e criando subpastas

---
+++ arquivos.java/Progam26.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Progam26 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		File path = new File(strPath);
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("Folders:");
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		System.out.println();
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("Files:");
+		for(File file : files) {
+			System.out.println(file);
+		}
+		System.out.println();
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: "+success);
+		sc.close();
+	}
+}
