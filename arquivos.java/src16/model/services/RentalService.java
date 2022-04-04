34681ae4f9d20ffe5170bdcf270f60cbb64dce6c
Samuel Fomseca <samueljosef3@gmail.com>
2022-04-04 00:17:09 -0300

    Problema de aluguel de carro sem interface

---
+++ arquivos.java/src16/model/services/RentalService.java
@@ -0,0 +1,35 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private double pricePerDay;
+	private double pricePerHour;
+	private BrazilTaxService taxService;
+
+	public RentalService(double pricePerDay, double pricePerHour, BrazilTaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice(CarRental carRental) {
+		long ti = carRental.getStart().getTime();
+		long tf = carRental.getFinish().getTime();
+
+		double hours = (double) (tf - ti) / 1000 / 60 / 60;
+
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+
+		double tax = taxService.tax(basicPayment);
+
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
