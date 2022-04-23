717fa25583f15aa6c646fbc29e16f3ae06d9b161
Samuel Fomseca <samueljosef3@gmail.com>
2022-04-23 12:27:18 -0300

    WildCard Bounded (coringa delimitado

---
+++ arquivos.java/src23/application/Progam28.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+
+public class Progam28 {
+
+	public static void main(String[] args) {
+
+		List<Shape> myShape = new ArrayList<>();
+		myShape.add(new Rectangle(3.0, 2.0));
+		myShape.add(new Circle(2.0));
+		
+		List<Circle> myCircles = new ArrayList<>();
+		myCircles.add(new Circle(2.0));
+		myCircles.add(new Circle(3.0));
+		
+		System.out.println("Total area: " + totalArea(myShape));
+		System.out.println("Total area: " + totalArea(myCircles));
+	}
+	
+	public static double totalArea(List<? extends Shape> list) {
+		double sum = 0.0;
+		for(Shape s : list) {
+			sum += s.area();
+		}
+		return sum;
+	}
+}
