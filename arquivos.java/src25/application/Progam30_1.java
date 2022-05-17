161af50463b1bb1e44c43f94f72164f529fb806f
Samuel Fomseca <samueljosef3@gmail.com>
2022-05-17 09:10:25 -0300

    Log: contar os users que acessam um site

---
+++ arquivos.java/src25/application/Progam30_1.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
+
+public class Progam30_1 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			Set<LogEntry> set = new HashSet<>();
+			
+			String line = br.readLine();
+			while(line != null) {
+				String[] fields = line.split(" ");
+				String userName = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				set.add(new LogEntry(userName, moment));
+				line = br.readLine();
+			}
+			
+			System.out.println("Total user: " + set.size());
+		
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
+	}
+}
