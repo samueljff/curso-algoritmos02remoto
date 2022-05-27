26e01c302a6e1f8d81040695e3dff0a3ac13756e
Samuel Fomseca <samueljosef3@gmail.com>
2022-05-27 11:13:59 -0300

    Comparator: expressões lambda melhorada

---
+++ arquivos.java/src27.4/application/Progam33.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import entities.Product;
+
+public class Progam33 {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		// expressão lambda (função anonima) também chamada de arrow functions
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+
+		list.sort(comp);
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
