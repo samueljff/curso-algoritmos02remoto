e3db3d1411eb33e54aca39d06b37c9eb482f41e4
Samuel Fomseca <samueljosef3@gmail.com>
2022-05-29 17:49:42 -0300

    Interface funcional: lambda inline

---
+++ arquivos.java/src29.4/application/Program33.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Predicate;
+
+import entities.Product;
+
+public class Program33 {
+
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		double min = 100.00;
+		list.removeIf(p -> p.getPrice() >= min);
+		
+		for(Product p1 : list) {
+			System.out.println(p1);
+		}
+	}
+}
