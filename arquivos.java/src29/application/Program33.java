d41b1792fa35482882fbbcb048942ffaa66f79a7
Samuel Fomseca <samueljosef3@gmail.com>
2022-05-29 17:15:15 -0300

    Interface Funcional: v1

---
+++ arquivos.java/src29/application/Program33.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.ProductPredicate;
+
+public class Program33 {
+
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		list.removeIf(new ProductPredicate());
+		
+		for(Product p1 : list) {
+			System.out.println(p1);
+		}
+	}
+}
