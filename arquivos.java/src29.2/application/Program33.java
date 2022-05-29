cce5099672702b43481ffc6cd79a559498bfd435
Samuel Fomseca <samueljosef3@gmail.com>
2022-05-29 17:31:29 -0300

    Interface funcional: v3 method non static

---
+++ arquivos.java/src29.2/application/Program33.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+
+public class Program33 {
+
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		list.removeIf(Product::nonBooleanStacticPreditcate);
+		
+		for(Product p1 : list) {
+			System.out.println(p1);
+		}
+	}
+}
